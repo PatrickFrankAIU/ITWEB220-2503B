777be939fcad8fedb0b6be4e564be0b6b54449df
Pat Frank <pfrank@aiufl.edu>
2025-06-30 19:11:57 -0400

    uploading issue 2

---
+++ Pat/Issue2/main.js
@@ -0,0 +1,7 @@
+// ask user for age in years
+let ageYears = prompt("How old are you (in years)?");
+
+// convert to days
+let ageDays = ageYears * 365;
+
+console.log("You are approximately " + ageDays + " days old.");
