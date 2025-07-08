baf07cf3e76ec6e4008c94b3d6457437568a4ee9
Furkan Vardali <furkan_vardali@hotmail.com>
2025-07-07 21:17:00 -0400

    uploding issue 6

---
+++ Furkan/issue6/main.js
@@ -0,0 +1,5 @@
+let tempCelcius = prompt("What is the temperature in Celcius");
+
+let convertFahrenheit = (tempCelcius * 9/5) + 32;
+
+console.log(`${tempCelcius} Celcius is ${convertFahrenheit} Fahrenheit`);
