a43f0b4f56d9fc87bb3d11b20e8ca7d8615aed8e
Furkan Vardali <furkan_vardali@hotmail.com>
2025-07-07 21:50:20 -0400

    Uploding issue 8

---
+++ Furkan/issue8/main.js
@@ -0,0 +1,12 @@
+let input = Number(prompt("What is the number?"));
+
+if (input > 0){
+    console.log("Number is psotive");
+}
+else if(input<0){
+    console.log("Number is negatif");
+
+}
+else if(input == 0){
+    console.log("Number is 0");
+}
