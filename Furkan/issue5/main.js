f27324d07c3bc75a83490bb51033dae626e4ce61
Furkan Vardali <furkan_vardali@hotmail.com>
2025-07-07 21:09:40 -0400

    Uploding issue 5

---
+++ Furkan/issue5/main.js
@@ -0,0 +1,6 @@
+let input1 = Number(prompt("Pls type first number!"));
+let input2 = Number(prompt("PLs type second number"));
+
+let answer = input1 % input2;
+
+console.log(`Reminder of this division is: ${answer}`);
